002da130da468f60e9bae1350cc6bdd0ed0c28dc
Your Name <you@example.com>
2026-03-21 11:14:30 +0100

    Removed unused code

--- src/mac-listen.cpp
+++ src/mac-listen.cpp
@@ -1,14 +1,7 @@
 #include <IOKit/hidsystem/IOHIDLib.h>
-#include <IOKit/hidsystem/ev_keymap.h>
+#include <IOKit/hidsystem/IOLLEvent.h>
 #include <IOKit/IOKitLib.h>
 
 int main() {
-    io_service_t service = IOServiceGetMatchingService(
-        kIOMainPortDefault, IOServiceMatching("IOHIDSystem"));
-    
-    io_connect_t connect;
-    IOServiceOpen(service, mach_task_self(), kIOHIDParamConnectType, &connect);
-    IOObjectRelease(service);
-
-    IOServiceClose(connect);
+    io_service_t service = IOServiceGetMatchingService(kIOMainPortDefault, IOServiceMatching("IOHIDSystem"));
 }
